262ff106e7663e73ff7f15655bdc4738ee796a17
Boyalone222 <annakeyxr@gmail.com>
2026-02-28 07:06:23 -0800

    update

--- app.js
+++ app.js
@@ -6,11 +6,22 @@ console.log(student);
 
 
 function greet (FullName) {
-    console.log('my', + 'is' + FullName );
+    console.log('my name is ' + FullName );
 }
 
-greet(Toheeb);
+greet("Toheeb");
 
-class car {
-    
+class Car {
+   constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  } 
+
+  displayProduct(){
+
+    console.log('product: ' + this.name);
+    console.log('price: ' + this.price);
+  }
 }
+const product = new Car("Benz",'40milliom');
+product.displayProduct();
