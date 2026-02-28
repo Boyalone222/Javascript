32528bccbd872d2281a61f1235a65ba9fbe24563
Boyalone222 <annakeyxr@gmail.com>
2026-02-27 20:30:28 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Hello World!
+var name = 'jasayo'
+let age = 30;
+student =  name + age
+console.log(student);
+
+
+function greet (FullName) {
+    console.log('my', + 'is' + FullName );
+}
+
+greet(Toheeb);
+
+class car {
+    
+}
